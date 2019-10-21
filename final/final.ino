d548cb119e000c2c3a034a6c27a2b654d80bfcaf
Lourival Alexandre <lourivalalee@gmail.com>
2019-10-21 16:49:44 -0300

    agr foisse

--- final/final.ino
+++ final/final.ino
@@ -14,8 +14,8 @@
 #define motorET PORTG1 //Arduino - 40
 #define motorDF PORTL7 //Arduino - 42
 #define motorDT PORTL5 //Arduino - 44
-#define velocidadeME PORTB5 //Arduino - 11
-#define velocidadeMD PORTB6 //Arduino - 12
+#define velocidadeME PORTB4 //Arduino - 10
+#define velocidadeMD PORTH6 //Arduino - 9
 #define botao PORTB3 // Arduino - 50
 #define potenciometro PORTF0 // Arduino - A0
 
@@ -27,11 +27,11 @@
 
 void adc_init()
 {
-    // AREF = AVcc
-    ADMUX = (1<<REFS0);
-    // ADC Enable and prescaler of 128
-    // 16000000/128 = 125000
-    ADCSRA = (1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
+  // AREF = AVcc
+  ADMUX = (1 << REFS0);
+  // ADC Enable and prescaler of 128
+  // 16000000/128 = 125000
+  ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
 }
 
 uint16_t myAnalogRead(uint8_t ch)
@@ -40,155 +40,169 @@ uint16_t myAnalogRead(uint8_t ch)
   // ANDing with ’7′ will always keep the value
   // of ‘ch’ between 0 and 7
   ch &= 0b00000111;  // AND operation with 7
-  ADMUX = (ADMUX & 0xF8)|ch; // clears the bottom 3 bits before ORing
- 
+  ADMUX = (ADMUX & 0xF8) | ch; // clears the bottom 3 bits before ORing
+
   // start single convertion
   // write ’1′ to ADSC
-  ADCSRA |= (1<<ADSC);
- 
+  ADCSRA |= (1 << ADSC);
+
   // wait for conversion to complete
   // ADSC becomes ’0′ again
   // till then, run loop continuously
-  while(ADCSRA & (1<<ADSC));
- 
+  while (ADCSRA & (1 << ADSC));
+
   return (ADC);
 }
 
-//uint16_t pwm = 0;
-
 int main() {
   Serial.begin(115200);
-  
+
   sei();
-  
+
   set_bit(DDRD, motorEF);
   set_bit(DDRG, motorET);
   set_bit(DDRL, motorDF);
   set_bit(DDRL, motorDT);
   set_bit(DDRB, velocidadeME);
-  set_bit(DDRB, velocidadeMD);
+  set_bit(DDRH, velocidadeMD);
   reset_bit(DDRF, potenciometro);
-  
+  reset_bit(DDRB, botao);
+  set_bit(PORTB, botao);
+
   adc_init();
 
-  /*TCCR0A = (1<<COM0A1) | (1<<WGM00) | (1<<WGM01);
-  TIMSK0 = (1<<TOIE0);
-  OCR0A = (qlqrcoisaai/100.0) * 255;
-  TCCR0B = (1<<CS00);*/
-  
+  OCR2A = 0;
+  OCR2B = 0;
+  TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
+  TCCR2B = _BV(CS21);
   Ultrasonic ultrasonic1(pino_trigger1, pino_echo1);
   Ultrasonic ultrasonic2(pino_trigger2, pino_echo2);
 
 
   uint16_t val, pwm, aleatorio, estado = 0, lastTimeComando = 0;
-  int8_t comando = '2';
+  int8_t comando = '1';
+  bool power = false;
 
   while (1) {
-    
-    uint32_t sensor1, sensor2, microsec1, microsec2;
-
-    microsec1 = ultrasonic1.timing();
-    microsec2 = ultrasonic2.timing();
-    sensor1 = ultrasonic1.convert(microsec1, Ultrasonic::CM);
-    sensor2 = ultrasonic2.convert(microsec2, Ultrasonic::CM);
-
-    val = myAnalogRead(0);
-    pwm = map(val, 0, 1023, 0, 255);
-    Serial.println(pwm);
-    comando = Serial.available() > 0 ? Serial.read() : comando;
-
-    DynamicJsonBuffer jBuffer;
-    JsonObject &root = jBuffer.createObject();
-
-    root["Sensor1"] = sensor1;
-    root["Sensor2"] = sensor2;
-
-    root.printTo(Serial);
-    Serial.println();
-
-    if (comando == '1')
-    { //andando para frente
-
-      myDigitalWrite(PORTD, motorEF, 1);
-      myDigitalWrite(PORTL, motorDF, 1);
-      myDigitalWrite(PORTG, motorET, 0);
-      myDigitalWrite(PORTL, motorDT, 0);
-
-      analogWrite(velocidadeMD, pwm);
-      analogWrite(velocidadeME, pwm);
-      if (sensor1 < 15)
-      {
-        aleatorio = random(0, 3);
-        while (sensor1 < 15)
-        {
-          if (aleatorio == 1)
-          { //curva para esquerda
-            analogWrite(velocidadeMD, pwm * 0.1);
-            analogWrite(velocidadeME, pwm);
-            myDigitalWrite(PORTD, motorEF, 1);
-            myDigitalWrite(PORTL, motorDF, 1);
-            myDigitalWrite(PORTG, motorET, 0);
-            myDigitalWrite(PORTL, motorDT, 0);
-          }
-          else if (aleatorio == 2)
-          { //curva para direita
-            analogWrite(velocidadeMD, pwm * 0.1);
-            analogWrite(velocidadeME, pwm);
-            myDigitalWrite(PORTD, motorEF, 1);
-            myDigitalWrite(PORTL, motorDF, 1);
-            myDigitalWrite(PORTG, motorET, 0);
-            myDigitalWrite(PORTL, motorDT, 0);
-          }
-          microsec1 = ultrasonic1.timing();
-          sensor1 = ultrasonic1.convert(microsec1, Ultrasonic::CM);
-        }
-      }
+    //Serial.println(myDigitalRead(PINB, botao));
+    //Serial.println(power);
+    if (myDigitalRead(PINB, botao) == 0) {
+      //Serial.println("Aqui");
+      power = !power;
     }
-    else if (comando == '2')
-    {
-      myDigitalWrite(PORTG, motorET, 1);
-      myDigitalWrite(PORTL, motorDT, 1);
-      myDigitalWrite(PORTD, motorEF, 0);
-      myDigitalWrite(PORTL, motorDF, 0);
-      
-      analogWrite(velocidadeMD, pwm);
-      analogWrite(velocidadeME, pwm);
-      if (sensor2 < 15)
+    else {
+      if (power)
       {
-        aleatorio = random(0, 3);
-        while (sensor2 < 15)
+        uint32_t sensor1, sensor2, microsec1, microsec2;
+
+        microsec1 = ultrasonic1.timing();
+        microsec2 = ultrasonic2.timing();
+        sensor1 = ultrasonic1.convert(microsec1, Ultrasonic::CM);
+        sensor2 = ultrasonic2.convert(microsec2, Ultrasonic::CM);
+
+        val = myAnalogRead(0);
+        Serial.println(val);
+        pwm = map(val, 0, 1023, 0, 255);
+        Serial.println(pwm);
+        comando = Serial.available() > 0 ? Serial.read() : comando;
+
+        DynamicJsonBuffer jBuffer;
+        JsonObject &root = jBuffer.createObject();
+
+        root["Sensor1"] = sensor1;
+        root["Sensor2"] = sensor2;
+
+        root.printTo(Serial);
+        Serial.println();
+
+        if (comando == '1')
+        { //andando para frente
+
+          myDigitalWrite(PORTD, motorEF, 1);
+          myDigitalWrite(PORTL, motorDF, 1);
+          myDigitalWrite(PORTG, motorET, 0);
+          myDigitalWrite(PORTL, motorDT, 0);
+
+          OCR2A = pwm; // MOTOR ESQUERDO
+          OCR2B = pwm; // MOTOR DIREITO
+
+          if (sensor1 < 15)
+          {
+            aleatorio = random(0, 3);
+            while (sensor1 < 15)
+            {
+              if (aleatorio == 1)
+              { //curva para esquerda
+                OCR2A = pwm * 0.1; // MOTOR ESQUERDO
+                OCR2B = pwm; // MOTOR DIREITO
+                myDigitalWrite(PORTD, motorEF, 1);
+                myDigitalWrite(PORTL, motorDF, 1);
+                myDigitalWrite(PORTG, motorET, 0);
+                myDigitalWrite(PORTL, motorDT, 0);
+              }
+              else if (aleatorio == 2)
+              { //curva para direita
+                OCR2A = pwm; // MOTOR ESQUERDO
+                OCR2B = pwm * 0.1; // MOTOR DIREITO
+                myDigitalWrite(PORTD, motorEF, 1);
+                myDigitalWrite(PORTL, motorDF, 1);
+                myDigitalWrite(PORTG, motorET, 0);
+                myDigitalWrite(PORTL, motorDT, 0);
+              }
+              microsec1 = ultrasonic1.timing();
+              sensor1 = ultrasonic1.convert(microsec1, Ultrasonic::CM);
+            }
+          }
+        }
+        else if (comando == '2')
         {
-          if (aleatorio == 1)
-          { //curva para esquerda
-            analogWrite(velocidadeMD, pwm * 0.1);
-            analogWrite(velocidadeME, pwm);
-            myDigitalWrite(PORTG, motorET, 1);
-            myDigitalWrite(PORTL, motorDT, 1);
-            myDigitalWrite(PORTD, motorEF, 0);
-            myDigitalWrite(PORTL, motorDF, 0);
+          myDigitalWrite(PORTG, motorET, 1);
+          myDigitalWrite(PORTL, motorDT, 1);
+          myDigitalWrite(PORTD, motorEF, 0);
+          myDigitalWrite(PORTL, motorDF, 0);
+
+          OCR2A = pwm; // MOTOR ESQUERDO
+          OCR2B = pwm; // MOTOR DIREITO
+
+          if (sensor2 < 15)
+          {
+            aleatorio = random(0, 3);
+            while (sensor2 < 15)
+            {
+              if (aleatorio == 1)
+              { //curva para esquerda
+                OCR2A = pwm; // MOTOR ESQUERDO
+                OCR2B = pwm * 0.1; // MOTOR DIREITO
+                myDigitalWrite(PORTG, motorET, 1);
+                myDigitalWrite(PORTL, motorDT, 1);
+                myDigitalWrite(PORTD, motorEF, 0);
+                myDigitalWrite(PORTL, motorDF, 0);
+              }
+              else if (aleatorio == 2)
+              { //curva para direita
+                OCR2A = pwm * 0.1; // MOTOR ESQUERDO
+                OCR2B = pwm; // MOTOR DIREITO
+                myDigitalWrite(PORTG, motorET, 1);
+                myDigitalWrite(PORTL, motorDT, 1);
+                myDigitalWrite(PORTD, motorEF, 0);
+                myDigitalWrite(PORTL, motorDF, 0);
+              }
+              microsec2 = ultrasonic2.timing();
+              sensor2 = ultrasonic2.convert(microsec2, Ultrasonic::CM);
+            }
           }
-          else if (aleatorio == 2)
-          { //curva para direita
-            analogWrite(velocidadeMD, pwm * 0.1);
-            analogWrite(velocidadeME, pwm);
-            myDigitalWrite(PORTG, motorET, 1);
-            myDigitalWrite(PORTL, motorDT, 1);
-            myDigitalWrite(PORTD, motorEF, 0);
-            myDigitalWrite(PORTL, motorDF, 0);
-          }
-          microsec2 = ultrasonic2.timing();
-          sensor2 = ultrasonic2.convert(microsec2, Ultrasonic::CM);
         }
       }
     }
   }
 }
 
-/*ISR(TIMER0_OVF_vect)
-{
-  OCR0A = (pwm/100.0) * 255;
-  
-}*/
+/*ISR(TIMER1_OVF_vect)
+  {
+  myDigitalWrite(PORTB,velocidadeMD, 0);
+  myDigitalWrite(PORTB,velocidadeME, 0);
+  OCR2A = (pwm/100.0) * 255;
+  }*/
 
 /*
   int val;
